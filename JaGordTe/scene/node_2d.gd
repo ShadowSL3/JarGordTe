e5aaad37c7c6cf05d644fb6432721d5682954b67
ShadowSL3 <mizasp71@gmail.com>
2024-10-18 14:32:05 +0200

    Added Asesprite Wizard Redesign Stamina Added Timer

--- JaGordTe/scene/node_2d.gd
+++ JaGordTe/scene/node_2d.gd
@@ -6,7 +6,7 @@ extends Node2D
 @onready var effect_damage = $UI/Label/Stamina/Effect_Damage
 @onready var color_rect = $UI/Label/Stamina/Effect_Damage/ColorRect
 @onready var animation_player = $UI/Label/Stamina/Effect_Damage/ColorRect/AnimationPlayer
-var velocity_on_time = 1
+@onready var timer_reload_health = $Timer
 func _ready():
 	color_rect.visible = false
 	
@@ -14,12 +14,17 @@ func _input(event):
 	if event is InputEvent:
 		if event.is_action("W"):
 			stamina.value -= 0.77
-		if stamina.value <= 10:
+			if stamina.value < 0:
+				stamina.value = 0
+		if stamina.value <= 8:
 			color_rect.visible = true
 			animation_player.play("stamina_low_anim_1")
-			animation_player.speed_scale * velocity_on_time
-		if stamina.value <= 7.5:
-			animation_player.speed_scale * velocity_on_time + 1
+#			animation_player.speed_scale * velocity_on_time
+#		if stamina.value <= 7.5:
+#			animation_player.speed_scale
 			print("IS Number On stamina", "\n", stamina.value)
-		if stamina.value <= 0:
-			print("Your Stamina Is Low")
+			if stamina.value == 0:
+				timer_reload_health.start(1.1)
+				print("Wait Moment Reloading Health")
+				stamina.value += 50 
+				animation_player.stop()
